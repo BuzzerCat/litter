a1f1e86edba022d1da7b4317c86618f06f276055
BuzzerCat <buzzerthecat@gmail.com>
2014-09-21 09:59:30 -0500

    Returning a file instead of a single line of html
    What: returning a file instead of a single line of html
    
    Why: so that I can serve a full html page.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var http = require("http");
+var fs = require('fs');
+var index = fs.readFileSync('cat.html');
 
 var answer = function(request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<h1> You're a kitty</h1>");
+  response.write(index);
   response.end();
 }
 

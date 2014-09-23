c520e5d0178cb1dde142511c042e9702da5e5f36
BuzzerCat <buzzerthecat@gmail.com>
2014-09-23 08:27:23 -0500

    Editing the server so it can receive data sent
    What:  created an if statement that receives the data sent in a post request. Post requests send data in small packages. It's like somebody sending you one piece of a puzzle each day. The if statement receives all of these pieces of the puzzle and print some out to the consul.
    Why: centrally I am going to need to store tweets on the server. This is the first step towards doing so.

--- server.js
+++ server.js
@@ -4,16 +4,29 @@ var index = fs.readFileSync('cat.html');
 
 var answer = function(request, response) {
   console.log(request.method );
+
   if ( request.method === "POST"){
      console.log("postal")
+      request.on('data', function(chunk) {
+      console.log("Received body data:");
+      console.log(chunk.toString());
+    });
+    
+    request.on('end', function() {
+      // empty 200 OK response for now
+      response.writeHead(200, "OK", {'Content-Type': 'text/html'});
+      response.end();
+    });
+
   }
   else if(request.method === "GET"){
     console.log("ghetto");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(index);
+    response.end();
    }
 
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(index);
-  response.end();
+  
 }
 
 var server = http.createServer(answer);

0dd71b02f9ef40ea99578ca5acaf5dead44a953f
BuzzerCat <buzzerthecat@gmail.com>
2014-09-23 13:28:54 -0500

    Trying to read tweets from file.
    Specifically the last 20 tweets.

--- server.js
+++ server.js
@@ -30,7 +30,13 @@ var answer = function(request, response) {
   else if(request.method === "GET"){
     console.log("ghetto");
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(index);
+     response.write(header);
+     var scratches_array = require('fs').readFileSync('scratches.txt').toString().split('\n')
+     for(var i = scratches_array.length-1; i > scratches-array.length-20; i--)
+       { 
+         response.write("<p>" + scratches_array[i] + "</p>");
+       }
+    response.write(footer);
     response.end();
    }
 

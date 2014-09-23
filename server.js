4df665b8485ab58538dbe700ef08c4ce3eaf4e43
Tara Roys <tlroys@gmail.com>
2014-09-23 13:54:12 +0000

    Revert "Storing tweet data"
    This reverts commit d13dc0e59c6d61204c04cfd87e1679dbb7c38e5d.

--- server.js
+++ server.js
@@ -4,17 +4,13 @@ var index = fs.readFileSync('cat.html');
 
 var answer = function(request, response) {
   console.log(request.method );
-   var fullbody = "";
 
   if ( request.method === "POST"){
      console.log("postal")
-
       request.on('data', function(chunk) {
-         console.log("Received body data:");
-         fullbody += chunk.toString()
-         console.log(fullbody);
-         
-      });
+      console.log("Received body data:");
+      console.log(chunk.toString());
+    });
     
     request.on('end', function() {
       // empty 200 OK response for now
@@ -25,7 +21,7 @@ var answer = function(request, response) {
   }
   else if(request.method === "GET"){
     console.log("ghetto");
-    response.writeHead(200, {"Content-Type": "tex/html"});
+    response.writeHead(200, {"Content-Type": "text/html"});
     response.write(index);
     response.end();
    }

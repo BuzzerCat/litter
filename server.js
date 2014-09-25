4083687feb885b0a447ec22c861529db98d4e447
BuzzerCat <buzzerthecat@gmail.com>
2014-09-25 12:42:31 -0500

    Removing unnecessary logging data.
    I had lots of consuls it was cooked cluttering up the code. I got rid of them.

--- server.js
+++ server.js
@@ -9,27 +9,19 @@ var answer = function(request, response) {
   var fullbody = "";
 
   if ( request.method === "POST"){
-     //console.log("postal")
       request.on('data', function(chunk) {
-        //console.log("Received body data:");
         fullbody += chunk.toString();
       });
     
     request.on('end', function() {
-      // empty 200 OK response for now
-      //response.writeHead(200, "OK", {'Content-Type': 'text/html'});
-      //response.end();
       var decodedbody = querystring.parse(fullbody);
-      //console.log(decodedbody.scratch);
       fs.appendFile('scratches.txt', decodedbody.scratch+"\n", function (err) {
   if (err) throw err;
-    //console.log('The "data to append" was appended to file!');
-});
+    });
     });
 
   }
   else if(request.method === "GET"){
-    //console.log("ghetto");
     response.writeHead(200, {"Content-Type": "text/html"});
      response.write(header);
      var scratches_array = require('fs').readFileSync('scratches.txt').toString().split('\n')

da0817d34fb769a3c9c3fefd46e5a376c552916d
BuzzerCat <buzzerthecat@gmail.com>
2014-09-25 17:13:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var answer = function(request, response) {
   else if(request.method === "GET"){
     response.writeHead(200, {"Content-Type": "text/html"});
      response.write(header);
-     var scratches_array = require('fs').readFileSync('scratches.txt').toString().split('\n')
+     var scratches_array = fs.readFileSync('scratches.txt').toString().split('\n')
      for(var i = scratches_array.length - 1; i > scratches_array.length - 20; i--)
        { 
          response.write("<p>" + scratches_array[i] + "</p>");
